70562f5f0e5be461c43c7d6601ed9d1bff68c136
ChristopherLandaverde <christopherlandaverde@gmail.com>
2025-09-13 21:09:45 -0400

    Remove .cursorrules file - not needed for portfolio

--- models/staging/stg_reviews_test.sql
+++
@@ -1,9 +0,0 @@
--- Simple test model to verify dbt is working
-select 
-    reviewid as review_id,
-    artist,
-    title as album_name,
-    score,
-    pub_date as review_date
-from {{ source('raw_pitchfork', 'reviews') }}
-limit 10
